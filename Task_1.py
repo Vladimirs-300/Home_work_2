9475cbdd120808038243735ee96907a332db13bf
vladimir <slr-klesks-300@yandex.ru>
2022-12-10 01:46:50 +1000

    выполнено

--- Task_1.py
+++ Task_1.py
@@ -1,4 +1,4 @@
-# Задача 1. 
+# Задача 1.
 # Напишите программу, которая принимает на вход
 # большое число и показывает его цифру.
 # Без работы с последовательностями строк.
@@ -7,9 +7,10 @@
 # - 0,67 -> 13
 # - 198,45 -> 27
 
-num = input("Enter the number: ")
+number = input('Введите число: ')
+
 sum = 0
-for i in num:
-    if i!=".":
-        sum = sum + int(i)
-print(f"The sum of the {num} is: ", sum)
+for i in number:
+    if i != '.':
+        sum += int(i)
+print(sum)
